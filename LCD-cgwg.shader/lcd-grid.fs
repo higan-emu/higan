49b48b54a48f018a605264340e048323df1408de
hunterk <hunter_kaller@yahoo.com>
2015-06-01 12:45:33 -0500

    update cgwg's LCD shader to latest

--- LCD-cgwg.shader/lcd-grid.fs
+++ LCD-cgwg.shader/lcd-grid.fs
@@ -1,8 +1,15 @@
 #version 150
 
-#define round(x) floor( (x) + 0.5 )
-#define gamma 2.2
-#define TEX2D(c) pow(texture2D(source[0], (c)), vec4(gamma))
+#in red
+#in green
+#in blue
+#in gain
+#in gamma
+#in blacklevel
+#in ambient
+#in BGR
+
+#define outgamma 2.2
 
 uniform sampler2D source[];
 uniform vec4 sourceSize[];
@@ -14,59 +21,68 @@ in Vertex {
 
 out vec4 fragColor;
 
-float intsmear_func(float z)
+#define fetch_offset(coord,offset) (pow(vec3(gain) * texelFetchOffset(source[0], (coord), 0, (offset)).rgb + vec3(blacklevel), vec3(gamma)) + vec3(ambient))
+
+// integral of (1 - x^2 - x^4 + x^6)^2
+const float coeffs_x[] = float[](1.0, -2.0/3.0, -1.0/5.0, 4.0/7.0, -1.0/9.0, -2.0/11.0, 1.0/13.0);
+// integral of (1 - 2x^4 + x^6)^2
+const float coeffs_y[] = float[](1.0,      0.0, -4.0/5.0, 2.0/7.0,  4.0/9.0, -4.0/11.0, 1.0/13.0); 
+float intsmear_func(float z, float coeffs[7])
 {
   float z2 = z*z;
-  float z4 = z2*z2;
-  float z8 = z4*z4;
-  return z - 2.0/3.0*z*z2 - 1.0/5.0*z*z4 + 4.0/7.0*z*z2*z4 - 1.0/9.0*z*z8
-    - 2.0/11.0*z*z2*z8 + 1.0/13.0*z*z4*z8;
+  float zn = z;
+  float ret = 0.0;
+  for (int i = 0; i < 7; i++) {
+    ret += zn*coeffs[i];
+    zn *= z2;
+  }
+  return ret;
 }
-float intsmear(float x, float dx)
+float intsmear(float x, float dx, float d, float coeffs[7])
 {
-  const float d = 1.5;
-  float zl = clamp((x-dx)/d,-1.0,1.0);
-  float zh = clamp((x+dx)/d,-1.0,1.0);
-  return d * ( intsmear_func(zh) - intsmear_func(zl) )/(2.0*dx);
+  float zl = clamp((x-dx*0.5)/d,-1.0,1.0);
+  float zh = clamp((x+dx*0.5)/d,-1.0,1.0);
+  return d * ( intsmear_func(zh,coeffs) - intsmear_func(zl,coeffs) )/dx;
 }
 
 void main()
 {
   vec2 texelSize = 1.0 / sourceSize[0].xy;
-  vec2 subtexelSize = texelSize / vec2(3.0,1.0);
   vec2 range = sourceSize[0].xy / (targetSize.xy * sourceSize[0].xy);
 
-  float left   = texCoord.x - texelSize.x*0.5;
-  float top    = texCoord.y + range.y;
-  float right  = texCoord.x + texelSize.x*0.5;
-  float bottom = texCoord.y - range.y;
+  vec3 cred   = pow(red,   vec3(outgamma));
+  vec3 cgreen = pow(green, vec3(outgamma));
+  vec3 cblue  = pow(blue,  vec3(outgamma));
 
-  vec4 lcol, rcol;
-  float subpix = mod(texCoord.x/subtexelSize.x+1.5,3.0);
-  float rsubpix = range.x/subtexelSize.x;
-  lcol = vec4(intsmear(subpix+1.0,rsubpix),intsmear(subpix    ,rsubpix),
-	      intsmear(subpix-1.0,rsubpix),0.0);
-  rcol = vec4(intsmear(subpix-2.0,rsubpix),intsmear(subpix-3.0,rsubpix),
-	      intsmear(subpix-4.0,rsubpix),0.0);
+  ivec2 tli = ivec2(floor(texCoord/texelSize-vec2(0.4999)));
 
-  vec4 topLeftColor     = TEX2D((floor(vec2(left, top)     / texelSize) + 0.5) * texelSize) * lcol;
-  vec4 bottomRightColor = TEX2D((floor(vec2(right, bottom) / texelSize) + 0.5) * texelSize) * rcol;
-  vec4 bottomLeftColor  = TEX2D((floor(vec2(left, bottom)  / texelSize) + 0.5) * texelSize) * lcol;
-  vec4 topRightColor    = TEX2D((floor(vec2(right, top)    / texelSize) + 0.5) * texelSize) * rcol;
+  vec3 lcol, rcol;
+  float subpix = (texCoord.x/texelSize.x - 0.4999 - float(tli.x))*3.0;
+  float rsubpix = range.x/texelSize.x * 3.0;
+  lcol = vec3(intsmear(subpix+1.0,rsubpix, 1.5, coeffs_x),
+              intsmear(subpix    ,rsubpix, 1.5, coeffs_x),
+              intsmear(subpix-1.0,rsubpix, 1.5, coeffs_x));
+  rcol = vec3(intsmear(subpix-2.0,rsubpix, 1.5, coeffs_x),
+              intsmear(subpix-3.0,rsubpix, 1.5, coeffs_x),
+              intsmear(subpix-4.0,rsubpix, 1.5, coeffs_x));
+#ifdef BGR
+  lcol.rgb = lcol.bgr;
+  rcol.rgb = rcol.bgr;
+#endif
+  float tcol, bcol;
+  subpix = texCoord.y/texelSize.y - 0.4999 - float(tli.y);
+  rsubpix = range.y/texelSize.y;
+  tcol = intsmear(subpix    ,rsubpix, 0.63, coeffs_y);
+  bcol = intsmear(subpix-1.0,rsubpix, 0.63, coeffs_y);
 
-  vec2 border = round(texCoord.st/subtexelSize);
-  float shry = 0.05;
-  vec2 bordert = clamp((border+vec2(0.0,+shry)) * subtexelSize,
-		       vec2(left, bottom), vec2(right, top));
-  vec2 borderb = clamp((border+vec2(0.0,-shry)) * subtexelSize,
-		       vec2(left, bottom), vec2(right, top));
-  float totalArea = 2.0 * range.y;
+  vec3 topLeftColor     = fetch_offset(tli, ivec2(0,0)) * lcol * vec3(tcol);
+  vec3 bottomRightColor = fetch_offset(tli, ivec2(1,1)) * rcol * vec3(bcol);
+  vec3 bottomLeftColor  = fetch_offset(tli, ivec2(0,1)) * lcol * vec3(bcol);
+  vec3 topRightColor    = fetch_offset(tli, ivec2(1,0)) * rcol * vec3(tcol);
 
-  vec4 averageColor;
-  averageColor  = ((top - bordert.y)    / totalArea) * topLeftColor;
-  averageColor += ((borderb.y - bottom) / totalArea) * bottomRightColor;
-  averageColor += ((borderb.y - bottom) / totalArea) * bottomLeftColor;
-  averageColor += ((top - bordert.y)    / totalArea) * topRightColor;
+  vec3 averageColor = topLeftColor + bottomRightColor + bottomLeftColor + topRightColor;
 
-fragColor = pow(averageColor,vec4(1.0/gamma));
+  averageColor = mat3x3(cred, cgreen, cblue) * averageColor;
+
+  fragColor = vec4(pow(averageColor,vec3(1.0/outgamma)),0.0);
 }
