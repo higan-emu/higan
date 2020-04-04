fb53be072c57952e6eb27cc56ab668d63b53000f
MerryMage <MerryMage@users.noreply.github.com>
2020-04-04 17:52:37 +0100

    cirrus: Upload aritfacts upon GitHub release

---
+++ .cirrus.upload.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+echo "Uploading..."
+name="higan-$CIRRUS_TAG-$CIRRUS_OS.zip"
+curl -X POST \
+    --data-binary @higan-nightly.zip \
+    --header "Authorization: token $GITHUB_TOKEN" \
+    --header "Content-Type: application/octet-stream" \
+    --output /dev/null \
+    --write-out '%{http_code}' \
+    "https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
