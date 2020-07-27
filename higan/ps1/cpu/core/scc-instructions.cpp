59198bb43f583b68e80f78ad2308cb9f346dab17
Luke Usher <luke.usher@outlook.com>
2020-07-27 14:45:57 +0100

    ps1: fix rfe instruction (it should not alter 'old interrupt' flags)

--- higan/ps1/cpu/core/scc-instructions.cpp
+++ higan/ps1/cpu/core/scc-instructions.cpp
@@ -9,5 +9,4 @@ auto CPU::instructionMTC0(cu32& rt, u8 rd) -> void {
 auto CPU::instructionRFE() -> void {
   scc.status.frame[0] = scc.status.frame[1];
   scc.status.frame[1] = scc.status.frame[2];
-  scc.status.frame[2] = {};
 }
