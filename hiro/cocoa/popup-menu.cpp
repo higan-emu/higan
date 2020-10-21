3baea121f8caec91d7287f1f88c5ad27eb5b8c67
Sintendo <bram.speeckaert@gmail.com>
2020-10-22 00:54:57 +0200

    hiro/cocoa: Implement PopupMenu
    Mostly copied from the regular Menu implementation.

--- hiro/cocoa/popup-menu.cpp
+++ hiro/cocoa/popup-menu.cpp
@@ -1,20 +1,58 @@
 #if defined(Hiro_PopupMenu)
 
+@implementation CocoaPopupMenu : NSMenuItem
+
+-(id) initWith:(hiro::mPopupMenu&)menuReference {
+  if(self = [super initWithTitle:@"" action:nil keyEquivalent:@""]) {
+    popupMenu = &menuReference;
+
+    cocoaPopupMenu = [[NSMenu alloc] initWithTitle:@""];
+    [self setSubmenu:cocoaPopupMenu];
+  }
+  return self;
+}
+
+-(NSMenu*) cocoaPopupMenu {
+  return cocoaPopupMenu;
+}
+
+@end
+
 namespace hiro {
 
 auto pPopupMenu::construct() -> void {
+  @autoreleasepool {
+    cocoaPopupMenu = [[CocoaPopupMenu alloc] initWith:self()];
+  }
 }
 
 auto pPopupMenu::destruct() -> void {
+  @autoreleasepool {
+    [[cocoaPopupMenu cocoaPopupMenu] release];
+  }
 }
 
 auto pPopupMenu::append(sAction action) -> void {
+  @autoreleasepool {
+    if(auto pAction = action->self()) {
+      [[cocoaPopupMenu cocoaPopupMenu] addItem:pAction->cocoaAction];
+    }
+  }
 }
 
 auto pPopupMenu::remove(sAction action) -> void {
+  @autoreleasepool {
+    if(auto pAction = action->self()) {
+      [[cocoaPopupMenu cocoaPopupMenu] removeItem:pAction->cocoaAction];
+    }
+  }
 }
 
 auto pPopupMenu::setVisible(bool visible) -> void {
+  if(!visible) return;
+
+  NSEvent* event = [[NSApplication sharedApplication] currentEvent];
+  [NSMenu popUpContextMenu:[cocoaPopupMenu cocoaPopupMenu] withEvent:event forView:nil];
 }
 
 }
