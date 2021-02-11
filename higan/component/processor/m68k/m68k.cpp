d27aa4cb6a12d73ada0ebcdddd300193b297cb82
mickagame <mickagame@hotmail.com>
2021-02-11 22:17:16 +0100

    Update m68k.cpp

--- higan/component/processor/m68k/m68k.cpp
+++ higan/component/processor/m68k/m68k.cpp
@@ -3,9 +3,6 @@
 
 namespace higan {
 
-enum : uint { Byte, Word, Long };
-enum : bool { Reverse = 1, Extend = 1, Hold = 1, Fast = 1 };
-
 #include "registers.cpp"
 #include "memory.cpp"
 #include "effective-address.cpp"
