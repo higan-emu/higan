9cd4b04ba631a2cb99c5160902d8fb7e1e820b30
qeeg <mszoopers@protonmail.com>
2021-03-06 22:37:10 -0600

    sfc: Report auto joypad polling in the HVBJOY register [asuramaru]

--- higan/sfc/cpu/io.cpp
+++ higan/sfc/cpu/io.cpp
@@ -31,7 +31,7 @@ auto CPU::readCPU(uint24 address, uint8 data) -> uint8 {
     return data;
 
   case 0x4212:  //HVBJOY
-    data.bit(0) = status.autoJoypadActive;
+    data.bit(0) = io.autoJoypadPoll && status.autoJoypadCounter < 33;
     data.bit(6) = hcounter() <= 2 || hcounter() >= 1096;  //Hblank
     data.bit(7) = vcounter() >= ppu.vdisp();              //Vblank
     return data;
